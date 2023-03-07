140688d14b620452c738a216a5c8b65e7ea19ed6
Anastasyia <moya_pochta_n.k@mail.ru>
2023-03-07 17:13:48 +0700

    Task_01

---
+++ src/main/java/Seminar_1/Main.java
@@ -0,0 +1,22 @@
+package Seminar_1;
+
+public class Main {
+    //Task_01
+    public static void main(String[] args) {
+        int numbers[] = new int[]{21, 7, 11, 3, 9, 19, 4};
+        int min = numbers[0];
+        int max = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) max = numbers[i];
+            else if (numbers[i] < min) min = numbers[i];
+        }
+        System.out.println("Maximum: " + max);
+        System.out.println("Minimum: " + min);
+
+    }
+
+}
+
+    //Task_02
+
