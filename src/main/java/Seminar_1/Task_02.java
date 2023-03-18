7b07c3ef1e4c55fce99fc86f42173d442dab591f
Anastasyia <moya_pochta_n.k@mail.ru>
2023-03-18 09:21:34 +0700

    Task_02

--- src/main/java/Seminar_1/Task_02.java
+++ src/main/java/Seminar_1/Task_02.java
@@ -1,14 +1,14 @@
-package Task_02;
+package Seminar_1;
 
 public class Task_02 {
     public static void main(String[] args) {
-        int[] array =  {3,2,2,3};
+        int[] array = {3, 2, 2, 3};
         int val = 3;
         int[] newArray = new int[array.length];
         int count = 0;
         int j = 0;
         for (int i = 0; i < array.length; i++) {
-            if (array[i] == val){
+            if (array[i] == val) {
                 count++;
             } else {
                 newArray[j] = array[i];
@@ -20,7 +20,7 @@ public class Task_02 {
             newArray[j] = val;
             j++;
         }
-        for (int i = 0; i <newArray.length; i++) {
+        for (int i = 0; i < newArray.length; i++) {
             System.out.printf("%d", newArray[i]);
         }
     }
